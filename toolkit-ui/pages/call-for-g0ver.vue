d9c94000b27d04aa1fafee301a7f379177622854
ddio <ddioibb@gmail.com>
2023-08-24 17:26:45 +0800

    feat: add basic landing page

---
+++ toolkit-ui/pages/call-for-g0ver.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.g0ver.center.ba.b--moon-gray.pa4.f4.lh-copy.mt4.mt6-ns
+  h1.f3.f1-ns.lh-copy.mt0 那個，我們還來不及做這頁，你願意幫幫我們ㄇ 🥹
+  p 我們正在徵求：
+  ul
+    li 前端工程師 - 打造順順的 PDF 閱讀器
+    li UX 設計師 - 理出順順的工人智慧、群眾外包流程
+    li AI 專家 - 訓練出能夠幫忙讀懂永續報告書的機器人
+    li 對環境永續的任何人 - 參與測試、提供意見
+  p 想知道更多？歡迎來：
+  ul
+    li 翻翻
+      nuxt-link.ml1(to="https://g0v.hackmd.io/@ddio-io/open-csr-report" target="blank" rel="noopener") 專案共筆
+    li 了解
+      nuxt-link.ml1(to="https://g0v.hackmd.io/@ddio-io/open-csr-report/%2Fq3MRTADVQaSziJ35foRn-g" target="blank" rel="noopener") 技術規劃
+    li
+      | 加入
+      nuxt-link.mh1(to="https://g0v.hackmd.io/@jothon/joing0vslack" target="blank" rel="noopener") g0v Slack #thaubing-esg
+      | 找我們聊天
+    li 參加
+      nuxt-link.ml1(to="https://g0v.hackmd.io/@ddio-io/open-csr-report/https%3A%2F%2Fg0v.hackmd.io%2Fskjho22fTHaQY_Ydgndw1g" target="blank" rel="noopener")
+        | 每週線上小聚
+</template>
+<style lang="scss" scoped>
+.g0ver {
+  width: 40rem;
+  max-width: calc(100vw - 4rem);
+}
+</style>
--- toolkit-ui/pages/index.vue
+++ toolkit-ui/pages/index.vue
@@ -1,50 +1,52 @@
 <template lang="pug">
-main.main
-  .pa2
-    .ba.b--gray.pa2
-      .main__control
-        control-panel(@report="changeReport" @page="changePage" @matched-pages="updateMatchedPages")
-  .main__reader
-    pdf-viewer(
-      v-if="report && reportPage"
-      :year="report.year"
-      :company="report.company"
-      :page="reportPage.page"
-      :highlight="reportPage.highlight"
-      :matched-pages="matchedPages"
-    )
+.landingWrapper.flex.items-center.ph3
+  .landing.center.mw8.lh-copy
+    h1.tc.f3.f1-ns 開放企業永續資料庫 - 人工智慧補完計畫
+    p.f4.tc.mw7.center
+      | 永續報告書，是企業對外溝通的重要管道，我們想要使用公開資料，加上企業發佈的永續報告書，建立開放、有共同標準的開放資料庫，
+      | 讓我們使用同一套標準，依據不同產業，驗證企業的永續報告書，促進產業轉型。
+    .flex.flex-wrap.justify-center.mt3
+      nuxt-link.landing__category.dim(to="/crowd-source")
+        h2.f3 路過的鄉民
+        p 一起來試試業界最尖端（？）的工人智慧吧！
+        p 你將協助我們標記永續報告書中的資料，以及驗證其他人標記的結果，不只能夠充實資料庫，也能讓我們將填答流程，做得更流暢。
+        p.landing__disclaimer 為了改善填答流程，我們將會收集你標注的資料，以及紀錄搜尋的歷史、瀏覽過的報告書頁次。由於這些資料無涉識別性，我們會將這些資料以 CC0 公開，讓大家一起來分析。
+        .b.f4.tr 開始標記 →
+      nuxt-link.landing__category.dim(to="/call-for-g0ver")
+        h2.f3 專業工人
+        p 受過工人培訓的你，來一起建立更完整的企業資料庫吧！
+        p 你將可以跳過群眾外包的流程，直接將資料貢獻給我們，讓資料庫更加完整。
+        p.landing__disclaimer 為了改善填答流程，我們將會收集你標注的資料，以及紀錄搜尋的歷史、瀏覽過的報告書頁次。由於這些資料無涉識別性，我們會將這些資料以 CC0 公開，讓大家一起來分析。
+        .b.f4.tr 開始標記 →
 </template>
-<script setup lang="ts">
-const report = ref(null)
-const reportPage = ref(null)
-const matchedPages = ref([])
-
-function changeReport (newReport) {
-  report.value = newReport
-  reportPage.value = { page: 1, highlight: '' }
+<style lang="scss" scoped>
+.landingWrapper {
+  min-height: calc(100vh - 5rem);
 }
 
-function changePage (newPage) {
-  reportPage.value = newPage
-}
+.landing {
+  &__category {
+    width: 25rem;
+    max-width: calc(100vw - 4rem);
+    border-radius: 0.5rem;
+    border: 1px solid #ccc;
+    padding: 2rem;
+    margin-bottom: 1.5rem;
+    color: #000;
+    text-decoration: none;
 
-function updateMatchedPages (matched) {
-  matchedPages.value = matched
-}
+    + .landing__category {
+      margin-left: 1.5rem;
+    }
 
-</script>
-<style lang="scss">
-body {
-  overflow: hidden;
-}
-.main {
-  display: grid;
-  grid-template-columns: 1fr 3fr;
+    p {
+      color: #444;
+    }
+  }
 
-  &__control {
-    height: calc(100vh - 2rem);
-    overflow-y: auto;
-    overflow-x: hidden;
+  p.landing__disclaimer {
+    color: #888;
+    font-size: 0.75rem;
   }
 }
 </style>
