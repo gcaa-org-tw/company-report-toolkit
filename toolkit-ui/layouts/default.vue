7673acb2d92acfb8768f1209055523e1a106e85d
ddio <ddioibb@gmail.com>
2023-08-24 23:17:01 +0800

    feat: add airtable API

--- toolkit-ui/layouts/default.vue
+++ toolkit-ui/layouts/default.vue
@@ -3,12 +3,10 @@
   .gcaa__header.flex.justify-between.ph2.pv3.bb.b--moon-gray.mb3
     nuxt-link.black.fw5.no-underline(to="/") 企業永續報告書閱讀器
     nuxt-link.gray.no-underline(to="/call-for-g0ver") 登入後台
-  slot
+  main
+    slot
 </template>
 <script setup lang="ts">
 </script>
 <style lang="scss">
-body {
-  overflow-y: auto;
-}
 </style>
--- toolkit-ui/pages/crowd-source/index.vue
+++ toolkit-ui/pages/crowd-source/index.vue
@@ -1,23 +1,37 @@
 <template lang="pug">
-main.main
-  .pa2
-    .ba.b--gray.pa2
-      .main__control
-        control-panel(@report="changeReport" @page="changePage" @matched-pages="updateMatchedPages")
-  .main__reader
-    pdf-viewer(
-      v-if="report && reportPage"
-      :year="report.year"
-      :company="report.company"
-      :page="reportPage.page"
-      :highlight="reportPage.highlight"
-      :matched-pages="matchedPages"
-    )
+.crowdLanding
+  .ma5(v-if="false")
+    input(v-model="userId" placeholder="User ID")
+    button(@click="getPendingFields") Test
+  .crowdInit(v-else)
+    .pa2
+      .ba.b--gray.pa2
+        .main__control
+          control-panel(@report="changeReport" @page="changePage" @matched-pages="updateMatchedPages")
+    .main__reader
+      pdf-viewer(
+        v-if="report && reportPage"
+        :year="report.year"
+        :company="report.company"
+        :page="reportPage.page"
+        :highlight="reportPage.highlight"
+        :matched-pages="matchedPages"
+      )
 </template>
 <script setup lang="ts">
+const airtable = useAirtable()
+
 const report = ref(null)
 const reportPage = ref(null)
 const matchedPages = ref([])
+
+const userId = ref('ddio')
+
+async function getPendingFields () {
+  if (userId.value) {
+    await airtable.getPendingFields(userId.value)
+  }
+}
 
 function changeReport (newReport) {
   report.value = newReport
@@ -35,9 +49,9 @@ function updateMatchedPages (matched) {
 </script>
 <style lang="scss">
 body {
-  overflow: hidden;
+  overflow-y: hidden;
 }
-.main {
+.crowdInit {
   display: grid;
   grid-template-columns: 1fr 3fr;
 
