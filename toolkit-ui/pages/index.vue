f78c44ed2199e3926725701e53fc96e07b7935cd
ddio <ddioibb@gmail.com>
2023-09-28 10:55:01 +0800

    feat: create sample test page

--- toolkit-ui/pages/index.vue
+++ toolkit-ui/pages/index.vue
@@ -6,14 +6,14 @@
       | 永續報告書，是企業對外溝通的重要管道，我們想要使用公開資料，加上企業發佈的永續報告書，建立開放、有共同標準的開放資料庫，
       | 讓我們使用同一套標準，依據不同產業，驗證企業的永續報告書，促進產業轉型。
     .flex.flex-wrap.justify-center.mt4
-      nuxt-link.landing__category.dim(to="/crowd-source")
+      a.landing__category.dim(href="#")
         h2.f3 我是
           span.underline 路過的鄉民
         p 一起來試試業界最尖端（？）的工人智慧吧！
         p 你將協助我們標記永續報告書中的資料，以及驗證其他人標記的結果，不只能夠充實資料庫，也能讓我們將填答流程，做得更流暢。
         p.landing__disclaimer 為了改善填答流程，我們將會收集你標注的資料，以及紀錄搜尋的歷史、瀏覽過的報告書頁次。由於這些資料無涉識別性，我們會將這些資料以 CC0 公開，讓大家一起來分析。
         .b.f4.tr 開始標記 →
-      nuxt-link.landing__category.dim(to="/call-for-g0ver")
+      nuxt-link.landing__category.dim(to="/profession")
         h2.f3 我是
           span.underline 專業工人
         p 受過工人培訓的你，來一起建立更完整的企業資料庫吧！
