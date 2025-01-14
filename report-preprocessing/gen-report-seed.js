ecacef013f8f8f2eb4156d344f260a8661d34ffd
ddio <ddioibb@gmail.com>
2025-01-14 10:00:56 +0800

    feat: ensure report-list.yml type and add spinner

--- report-preprocessing/gen-report-seed.js
+++ report-preprocessing/gen-report-seed.js
@@ -1,7 +1,10 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
 import path from 'path'
 import pdf2html from 'pdf2html'
 import yaml from 'js-yaml'
 import { prepareCompanyMap, extractPdfNameInfo } from './utils.js'
+import ora from 'ora'
 
 const pdfOptions = {
   maxBuffer: 1024 * 1024 * 50,
@@ -24,14 +27,22 @@ async function main() {
     process.exit(1)
   }
 
+  const spinner = ora('Preparing company map...').start()
   const companyMap = await prepareCompanyMap()
+  spinner.succeed('Company map prepared!')
+
   const reportsPerYear = {}
+  const totalReports = pdfFiles.length
+  let processed = 0
+  spinner.start(`Processing 0/${totalReports} PDFs...`)
 
   for (const pdfFile of pdfFiles) {
     const { companyId, year } = extractPdfNameInfo(pdfFile)
 
     try {
       const totalPages = await getTotalPages(pdfFile)
+      processed += 1
+      spinner.text = `Processing ${processed}/${totalReports}`
       const companyInfo = {
         id: companyId,
         name: companyMap.id[companyId],
@@ -48,16 +59,24 @@ async function main() {
     }
   }
 
+  spinner.succeed(`Processed ${processed}`)
+  spinner.start('Writing report list...')
+
   const output = []
 
   for (const year in reportsPerYear) {
     output.push({
-      year: year,
+      year: Number(year),
       reports: reportsPerYear[year]
     })
   }
 
-  console.log(yaml.dump(output))
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
+  fs.writeFileSync(
+    path.join(__dirname, 'report-list.yml'),
+    yaml.dump(output)
+  )
+  spinner.succeed('All done! Check report-list.yml for the result.')
 }
 
 main()
